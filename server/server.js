8e19191bb07819045b3da596743975d5525e0bd4
Daniel Juarez <danieljuarezp@hotmail.com>
2018-04-12 11:17:08 -0600

    Detectando usuarios desconectados

--- server/server.js
+++ server/server.js
@@ -20,6 +20,10 @@ let io = socketIO(server);
 
 io.on('connection', (client) => {
     console.log('Usuario conectado');
+
+    client.on('disconnect', ()=>{
+        console.log('Usuario desconectado');
+    });
 });
 
 server.listen(port, (err) => {
