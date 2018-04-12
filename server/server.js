bc85121ec2c77ab0bf814bc9e722ea119f0bfd98
Daniel Juarez <danieljuarezp@hotmail.com>
2018-04-12 11:34:52 -0600

    Comunicacion cliente - servidor

--- server/server.js
+++ server/server.js
@@ -19,10 +19,21 @@ app.use(express.static(publicPath));
 let io = socketIO(server);
 
 io.on('connection', (client) => {
+    
     console.log('Usuario conectado');
+
+    client.emit('Bienvenida', {
+        usuario: 'Admin',
+        mensaje: 'Bienvenido!'
+    });
 
     client.on('disconnect', ()=>{
         console.log('Usuario desconectado');
+    });
+
+    // Escuchar mensaje del cliente
+    client.on('enviarMensaje', (mensaje)=>{
+        console.log(mensaje)
     });
 });
 
